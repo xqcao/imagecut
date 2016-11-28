7f8221f69d7ea056ab7db94c3b404b9d90ba9eee
xiaoqiang cao <xiaoqiangcao@xiaoqiangs-MBP.hsd1.ca.comcast.net>
2016-11-28 00:34:20 -0800

    pic edit first demo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,request,render_template,send_file
 import os
+from flask import Flask,request,render_template,send_file
 from io import BytesIO
 from PIL import Image, ImageDraw
 
@@ -10,22 +10,7 @@ def brforeUpload():
     print('page load ...')
     message='please upload your image'
     return render_template('imgUploadMemory.html',msg=message)
-
-@app.route('/memoryimg',methods=['POST','GET'])
-def DomemoryProcess():
-    if request.method == 'POST' and 'photo' in request.files:
-        oneimage = request.files['photo']
-        # in_memory_io = BytesIO()
-        # ff.save(in_memory_io)
-        imageData =Image.open(oneimage)
-        
-        
-        draw = ImageDraw.Draw(imageData)
-        byte_io = BytesIO()
-        imageData.save(byte_io, 'PNG')
-        byte_io.seek(0)
-    return send_file(byte_io, mimetype='image/png')
-
+    
 @app.route('/passportimg',methods=['POST','GET'])
 def DoPassportMemory():
     # SQUARE_FIT_SIZE=300
@@ -36,19 +21,18 @@ def DoPassportMemory():
         imageData =Image.open(oneimage)
         iw,ih = imageData.size
         if iw>ih:
-            cp_img_h = int((sq/iw)*ih)
-            cp_img_w = sq
+            ex_p_x = int((iw-ih)/2)
+            im0 = imageData.crop((ex_p_x,0,(ih+ex_p_x),ih))
         else:
-            cp_img_w = int((sq/ih)*iw)
-            cp_img_h = sq
-        im = imageData.resize((cp_img_w,cp_img_h))
-        print('im size',im.size)
+            ex_p_y = int((ih-iw)/2)
+            im0 = imageData.crop((0,ex_p_y,iw,(iw+ex_p_y)))
+        im = im0.resize((600,600))
         bgbase.paste(im,(0,0))
-        bgbase.paste(im,(cp_img_w,0))
-        bgbase.paste(im,((cp_img_w*2),0))
-        bgbase.paste(im,(0,cp_img_h))
-        bgbase.paste(im,(cp_img_w,cp_img_h))
-        bgbase.paste(im,((cp_img_w*2),cp_img_h))
+        bgbase.paste(im,(600,0))
+        bgbase.paste(im,((600*2),0))
+        bgbase.paste(im,(0,600))
+        bgbase.paste(im,(600,600))
+        bgbase.paste(im,((600*2),600))
         
         draw = ImageDraw.Draw(bgbase)
         byte_io = BytesIO()
@@ -57,4 +41,5 @@ def DoPassportMemory():
     return send_file(byte_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    port=int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0',port=port)
